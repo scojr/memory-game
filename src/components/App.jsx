31c173dcd4aaf484f8cebca042e9a88470f88ee3
scojr <180873402+scojr@users.noreply.github.com>
2025-11-15 22:28:33 -0600

    Create Card component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Cards from './Card';
+import '../styles/App.css'
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <Cards num={9}></Cards>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "../styles/Card.css"
+
+
+
+
+export default function Cards({ num }) {
+  const [data, setData] = useState(null);
+  const url = (() => `https://dog.ceo/api/breeds/image/random/${num}`)();
+
+  useEffect(() => {
+    let ignore = false;
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        if (!ignore) {
+          setData(response);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
+  }, [url])
+  console.log(data);
+
+
+  const getImages = () => {
+    if (data) {
+      const images = [];
+      const image = (num) => <img className="card" key={num} src={data.message[num]} height="300" width="300" />
+      for (let i = 0; i < num; i++) {
+        images.push(image(i));
+      }
+      return images;
+    }
+  }
+
+  let images = getImages()
+
+  return (
+    <div className="cards">
+      {images}
+    </div>
+  )
+}
